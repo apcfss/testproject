b4655eec09a108216a63f4762661928e3ff5b8a3
balajee <balajeebalu22>
2021-04-10 16:47:15 +0530

    ist commit success

---
+++ IESPROJECT/src/main/java/com/app/ies/IesprojectApplication.java
@@ -0,0 +1,13 @@
+package com.app.ies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IesprojectApplication.class, args);
+	}
+
+}
